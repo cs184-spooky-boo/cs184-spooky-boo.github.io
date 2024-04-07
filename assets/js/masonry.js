feb38c73ed004d778e8eab8b614d0b5630793935
Christy Quang <christyquang@berkeley.edu>
2024-04-06 22:01:22 -0700

    pls

--- assets/js/masonry.js
+++ assets/js/masonry.js
@@ -1,12 +1,12 @@
-$(document).ready(function () {
-  // Init Masonry
-  var $grid = $(".grid").masonry({
-    gutter: 10,
-    horizontalOrder: true,
-    itemSelector: ".grid-item",
+$(document).ready(function() {
+    // Init Masonry
+    var $grid = $('.grid').masonry({
+      gutter: 10,
+      horizontalOrder: true,
+      itemSelector: '.grid-item',
+    });
+    // Layout Masonry after each image loads
+    $grid.imagesLoaded().progress( function() {
+      $grid.masonry('layout');
+    });
   });
-  // Layout Masonry after each image loads
-  $grid.imagesLoaded().progress(function () {
-    $grid.masonry("layout");
-  });
-});
